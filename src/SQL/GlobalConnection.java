359d7581675fecc611f001c85e507937d4d30139
Mikawi <90404558+Hassan220022@users.noreply.github.com>
2023-05-27 21:18:13 +0300

    Update GlobalConnection.java

--- src/SQL/GlobalConnection.java
+++ src/SQL/GlobalConnection.java
@@ -17,4 +17,45 @@ public class GlobalConnection {
         }
         return conn;
     }
+        private static String getQueryForID(String Table) {
+        String query;
+        switch (Table) {
+            case "Employees":
+                query = "SELECT Id FROM Employees WHERE username = ?";
+                break;
+            case "Marketing_Offers":
+                query = "SELECT Id FROM Marketing_Offers WHERE username = ?";
+                break;
+            case "Sales_Orders":
+                query = "SELECT Id FROM Sales_Orders WHERE username = ?";
+                break;
+            case "Inventory_Products":
+                query = "SELECT Id FROM Inventory_Products WHERE username = ?";
+                break;
+            case "Admin":
+                query = "SELECT Id FROM Admin WHERE username = ?";
+                break;
+            case "Marketing_Reports":
+                query = "SELECT Id FROM Marketing_Reports WHERE username = ?";
+                break;
+            default:
+                query = "SELECT Id FROM Users WHERE username = ?";
+                break;
+        }
+        return query;
+    }
+    public static public static int getID_data(String Table) throws SQLException {
+        try {
+            Connection conn = GlobalConnection.getConnection();
+            String query = getQueryForID(Table);
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("Id");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
