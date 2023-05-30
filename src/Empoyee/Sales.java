e19fade2299f25490edf2bb90c115d9eacf230d9
Mikawi <hassansonson2002@gmail.com>
2023-05-30 03:56:24 +0300

    update

--- src/Empoyee/Sales.java
+++ src/Empoyee/Sales.java
@@ -1,7 +1,6 @@
 package Empoyee;
 
 import java.sql.SQLException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,90 +10,53 @@ import User.User;
 class Sales extends User {
 
     private List<Product> products;
+    private Order order;
 
     public Sales(String name, String username, String password) {
         super(username, password, "Sales");
         this.products = new ArrayList<>();
     }
 
-    public String getUsername() {
-        return username;
+    public void SetUsername(String username) throws SQLException {
+        setUsername(username);
     }
 
-    public void searchProduct(String keyword) throws SQLException {
-        List<Product> searchResults = new ArrayList<>();
-        for (Product product : products) {
-            if (product.getName().contains(keyword)) {
-                searchResults.add(product);
-            }
-        }
-
-        if (searchResults.isEmpty()) {
-            System.out.println("No products found matching the keyword: " + keyword);
-        } else {
-            System.out.println("Search results for keyword: " + keyword);
-            for (Product product : searchResults) {
-                System.out.println(product);
-            }
-        }
+    public void SetPassword(String password) throws SQLException {
+        setPassword(password);
     }
 
-    public void listAllProducts() {
-        System.out.println("List of all available products:");
-        for (Product product : products) {
-            System.out.println(product);
-        }
+    public List<Product> SearchProduct(String keyword) throws SQLException {
+        return Product.searchProduct(keyword);
     }
 
-    public void makeOrder(Product product, int quantity) throws SQLException {
+    public List<Product> GetAllProduct() {
+        return Product.getAllProducts();
+    }
+
+    public void makeOrder(Product product, int quantity) throws SQLException { // TODO: implement this
         // Logic to create an order for the given product and quantity
-        // You can add your implementation here
-        System.out.println("Order placed: " + quantity + " " + product.getName());
+
     }
 
-    public void cancelOrder(Product product) throws SQLException {
-        // Logic to cancel an order for the given product
-        // You can add your implementation here
-        System.out.println("Order canceled: " + product.getName());
+    public void cancelOrder() throws SQLException {
+        order.cancelOrder();
     }
 
-    public void addProduct(Product product) throws SQLException {
-        products.add(product);
-        System.out.println("Product added: " + product.getName());
+    public void addProductOrder(Product product) throws SQLException {
+        order.addProduct(product);
     }
 
-    public void deleteProduct(Product product) throws SQLException {
-        products.remove(product);
-        System.out.println("Product deleted: " + product.getName());
+    public void deleteProduct(Product product) throws SQLException {// check if it updates with the database
+        order.removeProduct(product);
+        products.remove(product);// remove product from list
     }
 
-    public void updateProduct(Product product) throws SQLException, ParseException {
-        for (Product p : products) {
-            if (p.getId() == product.getId()) {
-                p.setName(product.getName());
-                p.setPrice(product.getPrice());
-                p.setQuantity(product.getQuantity());
-                p.setExpiryDate(product.getExpiryDate());
-                System.out.println("Product updated: " + p.getName());
-                return;
-            }
-        }
-        System.out.println("Product not found for update.");
+    public Product findProductById(int productId) throws SQLException {
+        return Product.findProductById(productId);
     }
 
-    public List<Product> getProducts() {
-        return products;
+    public double getTotalPrice() throws SQLException { // TODO: implement this
+        return order.getTotalPrice();
     }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    public double getTotalPrice() { // TODO: implement this
-        return 0;
-    }
-
-    public int getQuantity() { // TODO: implement this
-        return 0;
-    }
 }
